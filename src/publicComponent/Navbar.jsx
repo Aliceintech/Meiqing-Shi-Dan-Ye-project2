2a23e5681e5258eeaad5dbfec3dbc6c28c6080ba
danye <ye.da@northeastern.edu>
2023-11-11 23:26:28 -0800

    name

--- src/publicComponent/Navbar.jsx
+++ src/publicComponent/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css';
+import './Navbar.css';
 
 const Navbar = ({ showGameLink, showRulesLink }) => {
   return (
-    <nav className="navbar">
+    <nav className="Navbar">
       <Link to="/">Welcome</Link>
       {showGameLink && <Link to="/game">Play</Link>}
       {showRulesLink && <Link to="/rules">Rules</Link>}
